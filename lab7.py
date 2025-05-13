47d352bd4a81fc2b1eb15a5b9dbc83facc377dec
Jesse Han <jessehan.2001@gmail.com>
2025-05-13 11:20:21 -0400

    p1 progress

--- lab7.py
+++ lab7.py
@@ -9,22 +9,35 @@ from PIL import Image
 import numpy as np
 
 def ideal_low_pass(image, cutoff=30):
-    width = image.size[0]
-    height = image.size[1]
     img = image.copy()
-    pxl = img.load()
 
+    # Image -> array for np manipulation
     img_array = np.array(img)
+    
+    f_transform = np.fft.fft2(img_array)
+    f_shift = np.fft.fftshift(f_transform)
 
-    fft = np.fft.fft2(img_array)
-    fft_shifted = np.fft.fftshift(fft)
+    rows, cols = img_array.shape
+    mask = np.zeros((rows, cols), np.uint8)
+    for i in range(rows):
+        for j in range(cols):
+            if np.sqrt((i - (rows // 2))**2 + (j - (cols // 2))**2) <= cutoff:
+                mask[i, j] = 1
 
-    for i in range(width):
-        for j in range(height):
-            if np.sqrt() <= cutoff
+    f_shift_filtered = f_shift * mask
+
+    f_inverse_shift = np.fft.ifftshift(f_shift_filtered)
+    img_filtered = np.fft.ifft2(f_inverse_shift)
+    img_filtered = np.real(img_filtered)
+
+    img_filtered = (img_filtered - np.min(img_filtered)) / (np.max(img_filtered) - np.min(img_filtered)) * 255
+    img_filtered = np.uint8(img_filtered)
+    
+    return Image.fromarray(img_filtered)
 
 dog = Image.open('dog.png').convert('L')
 dog.save('grayscale_dog.png')
 
 lowpass_dog = ideal_low_pass(dog)
+lowpass_dog.save('lowpass_dog.png')
 
